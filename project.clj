ba6c36cb4f42508578e2d24fe5a6d34ab04677fa
Anthony Grimes <i@raynes.me>
2011-12-24 16:55:33 -0600

    Add some deps.

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject refheap "0.1.0"
   :description "This is like, totally a pastebin, dude."
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.3.0-alpha1"]]
+                 [noir "1.3.0-alpha1"]
+                 [congomongo "0.1.7"]
+                 [clj-config "0.2.0"]]
   :main refheap.server)
 
