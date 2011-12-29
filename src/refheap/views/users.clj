5c498f8c18746aba96bb2513ca4388de15253163
Anthony Grimes <i@raynes.me>
2011-12-29 09:05:12 -0600

    404 for non-existent users.

--- src/refheap/views/users.clj
+++ src/refheap/views/users.clj
@@ -8,36 +8,37 @@
             [noir.session :as session]))
 
 (defn user-page [user page]
-  (let [paste-count (users/count-user-pastes user)
-        you? (= user (:username (session/get :user)))]
-    (layout
-     [:div#user
-      (-> user users/get-user :email (gravatar :size 70) ph/image)
-      [:p
-       (if you?
-         "You have "
-         (str user " has "))
-       (users/count-user-pastes user {:private false})
-       " public "
-       (if you?
-         (str "and " (users/count-user-pastes user {:private true}) " private paste(s).")
-         "paste(s).")]
-      (for [{:keys [paste-id summary date private]} (users/user-pastes
-                                                     user page
-                                                     (when-not you? {:private false}))]
-        (list
-         [:span.header
-          (when private
-            (ph/image "/img/lock.png"))
-          (ph/link-to (str "/paste/" paste-id) paste-id)
-          (date-string date)]
-         [:div.syntax summary]
-         [:br]))
-      [:div.centered
-       (when-not (= 1 page)
-         [:a#newer.pagebutton {:href (str "/users/" user "?page=" (dec page))} "newer"])
-       (when-not (= page (users/count-pages paste-count))
-         [:a.pagebutton {:href (str "/users/" user "?page=" (inc page))} "older"])]])))
+  (when-let [user-data (users/get-user user)]
+    (let [paste-count (users/count-user-pastes user)
+          you? (= user (:username (session/get :user)))]
+      (layout
+       [:div#user
+        (-> user-data :email (gravatar :size 70) ph/image)
+        [:p
+         (if you?
+           "You have "
+           (str user " has "))
+         (users/count-user-pastes user {:private false})
+         " public "
+         (if you?
+           (str "and " (users/count-user-pastes user {:private true}) " private paste(s).")
+           "paste(s).")]
+        (for [{:keys [paste-id summary date private]} (users/user-pastes
+                                                       user page
+                                                       (when-not you? {:private false}))]
+          (list
+           [:span.header
+            (when private
+              (ph/image "/img/lock.png"))
+            (ph/link-to (str "/paste/" paste-id) paste-id)
+            (date-string date)]
+           [:div.syntax summary]
+           [:br]))
+        [:div.centered
+         (when-not (= 1 page)
+           [:a#newer.pagebutton {:href (str "/users/" user "?page=" (dec page))} "newer"])
+         (when-not (= page (users/count-pages paste-count))
+           [:a.pagebutton {:href (str "/users/" user "?page=" (inc page))} "older"])]]))))
 
 (defpage "/users/:user" {:keys [user page]}
   (user-page user (Long. (or page "1"))))
