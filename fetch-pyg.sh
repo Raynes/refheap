cdc3b9bb083b6bcc324d8268df608a936a341ecc
Anthony Grimes <i@raynes.me>
2011-12-30 13:37:52 -0600

    Fetch bitbucket pygments.

--- fetch-pyg.sh
+++ fetch-pyg.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-wget http://pypi.python.org/packages/source/P/Pygments/Pygments-1.4.tar.gz#md5=d77ac8c93a7fb27545f2522abe9cc462
-tar -xvf Pygments-1.4.tar.gz
-rm Pygments-1.4.tar.gz
-mv Pygments-1.4 resources/pygments
+hg clone http://dev.pocoo.org/hg/pygments-main resources/pygments
