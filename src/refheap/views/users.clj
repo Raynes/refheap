4a1f6b9ea998aea4afc2a26f77fc10ec8c32b951
Anthony Grimes <i@raynes.me>
2012-01-03 11:23:34 -0600

    More pagination fixes.

--- src/refheap/views/users.clj
+++ src/refheap/views/users.clj
@@ -3,7 +3,7 @@
         [refheap.views.common :only [layout avatar page-buttons]]
         [refheap.dates :only [date-string]]
         [noir.response :only [redirect]]
-        [refheap.models.paste :only [count-pages]])
+        [refheap.models.paste :only [count-pages proper-page]])
   (:require [refheap.models.users :as users]
             [hiccup.page-helpers :as ph]
             [noir.session :as session]))
@@ -22,21 +22,25 @@
 
 (defn user-page [user page]
   (when-let [user-data (users/get-user user)]
-    (let [you? (= user (:username (session/get :user)))]
-      (layout
-       [:div#user
-        (-> user-data :email (avatar 70))
-        [:p
-         (if you?
-           "You have "
-           (str user " has "))
-         (users/count-user-pastes user {:private false})
-         " public "
-         (if you?
-           (str "and " (users/count-user-pastes user {:private true}) " private paste(s).")
-           "paste(s).")]
-        (pastes (users/user-pastes user page (when-not you? {:private false})))
-        (page-buttons (str "/users/" user) (users/count-user-pastes user) 10 page)]))))
+    (let [you? (= user (:username (session/get :user)))
+          others (when-not you? {:private false})
+          paste-count (users/count-user-pastes user others)]
+      (if (> page (count-pages paste-count 10))
+        (redirect "/paste")
+        (layout
+         [:div#user
+          (-> user-data :email (avatar 70))
+          [:p
+           (if you?
+             "You have "
+             (str user " has "))
+           (users/count-user-pastes user {:private false})
+           " public "
+           (if you?
+             (str "and " (users/count-user-pastes user {:private true}) " private paste(s).")
+             "paste(s).")]
+          (pastes (users/user-pastes user page others))
+          (page-buttons (str "/users/" user) paste-count 10 page)])))))
 
 (defpage "/users/:user" {:keys [user page]}
-  (user-page user (Long. (or page "1"))))
+  (user-page user (proper-page (Long. (or page "1")))))
