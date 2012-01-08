5e0cca1d5542598d8e7d40ac8361fd9df4330f23
Alex McNamara <alex.mcnamara@gmail.com>
2012-01-07 20:05:47 -0500

    Updated version number. 1.0.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject refheap "0.1.0"
+(defproject refheap "1.0.0"
   :description "This is like, totally a pastebin, dude."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.3.0-alpha3"]
