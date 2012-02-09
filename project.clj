44d73514352bdd35f3f0126c7e6a718257725f6d
Anthony Grimes <i@raynes.me>
2012-02-09 09:10:49 -0600

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
 (defproject refheap "1.0.0"
   :description "This is like, totally a pastebin, dude."
+  :url "https://refheap.com"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [noir "1.3.0-alpha3"]
-                 [congomongo "0.1.7"]
+                 [noir "1.3.0-alpha8"]
+                 [congomongo "0.1.8"]
                  [clj-config "0.2.0"]
-                 [clj-http "0.2.6"]
+                 [clj-http "0.3.1"]
                  [clavatar "0.2.0"]
                  [clj-time "0.3.3"]
                  [amalloy/mongo-session "0.0.1"]]
