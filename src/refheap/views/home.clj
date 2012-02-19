7d8230d7304a8e2c72b7a71f6111dfbc97daa4eb
Anthony Grimes <i@raynes.me>
2012-02-19 15:32:42 -0600

    Remove spurious import.

--- src/refheap/views/home.clj
+++ src/refheap/views/home.clj
@@ -2,7 +2,7 @@
   (:use [refheap.views.common :only [layout]]
         [noir.response :only [redirect]]
         [noir.statuses :only [set-page!]]
-        [noir.core :only [defpartial defpage]]))
+        [noir.core :only [defpage]]))
 
 (defn not-found []
   (layout "<p class=\"header\">Insert fancy 404 image here.</p>"))
