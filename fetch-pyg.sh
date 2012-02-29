e76dfe5345b06660eb65e5261aef37e17f1a48ee
Anthony Grimes <i@raynes.me>
2012-02-29 03:35:01 -0600

    Use curl and tar instead of hg.

--- fetch-pyg.sh
+++ fetch-pyg.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-hg clone https://bitbucket.org/Raynes/pygments-main resources/pygments
+rm -rf resources/pygments
+curl https://bitbucket.org/birkenfeld/pygments-main/get/de3157655c32.tar.gz -o p.tar.gz
+tar -xf p.tar.gz
+mv birkenfeld-pygments-main-de3157655c32/ resources/pygments
+rm p.tar.gz
