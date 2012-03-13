7bfae06658a293fc9b209725d37fd5d477688e60
Anthony Grimes <i@raynes.me>
2012-03-12 19:52:07 -0500

    Some work towards integrating codemirror.
    Wrote a tomorrow night theme and got it stuffed into the page. Still ahve to
    tie it together.

--- resources/public/js/create.js
+++ resources/public/js/create.js
@@ -1,1 +1,11 @@
-$(document).ready(function() { $('select#language').chosen(); })
+$(document).ready(function() {
+
+  // Chosen
+  $('select#language').chosen();
+
+  // CodeMirror
+  var editor = CodeMirror.fromTextArea(document.getElementById('paste'), {
+    lineNumbers: true,
+    theme: 'cmtn',
+  })
+})
