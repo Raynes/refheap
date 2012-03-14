b6d46b379c1692b99b89a37b5012314d628f167a
Anthony Grimes <i@raynes.me>
2012-03-13 20:43:24 -0500

    Tie codemirror to the language dropdown.

--- resources/public/js/create.js
+++ resources/public/js/create.js
@@ -1,11 +1,71 @@
 $(document).ready(function() {
 
   // Chosen
-  $('select#language').chosen();
+  $('select#language').chosen()
+
+  var loaded = {}
+
+  function loadFn(mode) {
+    return function(cm) {
+      var isLoaded = loaded[mode]
+      var setMode = function () {
+        loaded[mode] = true
+        cm.setOption("mode", mode)
+      }
+      if (!isLoaded) {
+        $.getScript("/js/codemirror/mode/" + mode + "/" + mode + ".js", setMode)
+      } else {
+        setMode()
+      }
+    }
+  }
+
+  var langs = { "C++"             : loadFn("clike"),
+                "C"               : loadFn("clike"),
+                "Objective-C"     : loadFn("clike"),
+                "Clojure"         : loadFn("clojure"),
+                "CoffeeScript"    : loadFn("coffeescript"),
+                "CSS"             : loadFn("css"),
+                "Diff"            : loadFn("diff"),
+                "Go"              : loadFn("go"),
+                "Groovy"          : loadFn("groovy"),
+                "Haskell"         : loadFn("haskell"),
+                "Javascript"      : loadFn("javascript"),
+                "Lua"             : loadFn("lua"),
+                "Delphi"          : loadFn("pascal"),
+                "Perl"            : loadFn("perl"),
+                "PHP"             : loadFn("php"),
+                "Java Properties" : loadFn("properties"),
+                "Python"          : loadFn("python"),
+                "R"               : loadFn("r"),
+                "Ruby"            : loadFn("ruby"),
+                "Rust"            : loadFn("rust"),
+                "Scheme"          : loadFn("scheme"),
+                "Emacs Lisp"      : loadFn("scheme"),
+                "Smalltalk"       : loadFn("smalltalk"),
+                "Verilog"         : loadFn("verilog"),
+                "XML"             : loadFn("xml"),
+                "YAML"            : loadFn("yaml") }
+
+  function loadMode(selected, editor) {
+    lang = langs[selected]
+    if (lang) {
+      lang(editor)
+    } else {
+      editor.setOption("mode", null)
+    }
+  }
 
   // CodeMirror
   var editor = CodeMirror.fromTextArea(document.getElementById('paste'), {
     lineNumbers: true,
-    theme: 'cmtn',
+    theme: 'cmtn'
+  })
+
+  var selected = $("#language option:selected").text()
+  loadMode(selected, editor)
+
+  $("#language").change(function () {
+    loadMode($("#language option:selected").text(), editor)
   })
 })
