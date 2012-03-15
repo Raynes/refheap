6a635f0a623d31ffeb650601d355171ef9c2d7d4
Anthony Grimes <i@raynes.me>
2012-03-15 04:25:38 -0500

    Include tomorrow-night.css on the user page.
    Duh.

--- src/refheap/views/users.clj
+++ src/refheap/views/users.clj
@@ -26,7 +26,8 @@
                      "refheap/views/templates/userheader")
            :paste-buttons (page-buttons (str "/users/" user) total 20 page)
            :gravatar (avatar (:email user-data) 70)})
-        {:title (str user "'s pastes")}))))
+        {:file "refheap/views/templates/showhead"
+         :title (str user "'s pastes")}))))
 
 (defpage "/users/:user" {:keys [user page]}
   (user-page (.toLowerCase user) 
