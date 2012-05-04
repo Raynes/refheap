ace238292615a4d0848520942e5cf04318965b57
Anthony Grimes <i@raynes.me>
2012-05-04 01:59:54 -0500

    Fix bootstrap.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,10 @@
 SHA="34cc08a2c354"
 
 echo "Fetching Pygments..."
-curl https://bitbucket.org/birkenfeld/pygments-main/get/$SHA.zip -o pygments.zip
-unzip pygments.zip
+curl https://bitbucket.org/birkenfeld/pygments-main/get/$SHA.tar.gz -o pygments.tar.gz
+tar -xvzf pygments.tar.gz
 mv birkenfeld-pygments-main-$SHA resources/pygments
-rm pygments.zip
+rm pygments.tar.gz
 
 echo "Fetching submodules (codemirror)..."
 git submodule init
