dd7f15ad19bf08aa6484d81838e1bd29b52663a7
Anthony Grimes <i@raynes.me>
2012-05-04 22:26:58 -0500

    Return a proper www url.

--- src/refheap/models/api.clj
+++ src/refheap/models/api.clj
@@ -52,7 +52,7 @@
       (assoc :contents (:raw-contents paste))
       (assoc :user (when-let [user (:user paste)]
                      (:username (users/get-user-by-id user))))
-      (assoc :url (str "https://refheap.com/paste/" (:paste-id paste)))
+      (assoc :url (str "https://www.refheap.com/paste/" (:paste-id paste)))
       (dissoc :id :_id :raw-contents :summary)
       id->paste-id))
 
