feb3bccbd77be27f6e4e7d6e04f4625a68b560b3
William Kevin Manire <williamkmanire@gmail.com>
2012-05-13 03:18:04 -0400

    Updated refheap.js to current practices with jQuery and set up beginnings of a refheap javascript namespace.

--- resources/public/js/refheap.js
+++ resources/public/js/refheap.js
@@ -1,19 +1,28 @@
-$(document).ready(function(){
-  $("#signin").click(function(event) {
-    navigator.id.getVerifiedEmail(function(assertion) {
-      if (assertion) {
-        $.ajax({type: "POST",
-               url: "/user/verify",
-               data: { assertion: assertion},
-               dataType: "json",
-               success: function(data) {
-                 if (data) {
-                   $("#useri").html(data["login-html"]);
-                   $("body").html(data["chooselogin-html"])
-                 }
-               },
-        });
-      }
-    })
-  })
-});
+/*
+ * Refheap UI Enhancements
+ */
+
+$( function ( $, window ) {
+
+  var refheap = refheap || {};
+
+  /**
+   * Prompt the user for their BrowserID and verify via XHR POST.
+   */
+  refheap.signIn = function () {
+    navigator.id.get( function ( assertion ) {
+      $.post( "/user/verify", { assertion: assertion }, function ( data ) {
+        if ( data ) {
+          console.log( data );
+          $( "#useri" ).html( data["login-html"] );
+          $( "body" ).html( data["chooselogin-html"] );
+        }
+      });
+    });
+  };
+
+  $( function () {
+    $( "#signin" ).click( refheap.signIn );
+  });
+}( jQuery, window ));
+
