0b5261978086d7f6f336f2c370efcb2d99e42d71
Anthony Grimes <i@raynes.me>
2012-06-10 22:11:58 -0500

    Update some dependencies.

--- project.clj
+++ project.clj
@@ -3,14 +3,14 @@
   :url "https://refheap.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [stencil "0.2.0"]
-                 [noir "1.3.0-beta7"]
+                 [noir "1.3.0-beta8"]
                  [congomongo "0.1.9"]
                  [clj-config "0.2.0"]
                  [clj-http "0.4.0"]
                  [clavatar "0.2.0"]
                  [clj-time "0.3.3"]
                  [amalloy/mongo-session "0.0.1"]
-                 [conch "0.2.1"]
+                 [conch "0.3.0"]
                  [org.bovinegenius/exploding-fish "0.2.0"]]
   :main refheap.server)
 
