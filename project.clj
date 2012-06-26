4bd3b0a624866e44941a30e97f9c386980d85993
Anthony Grimes <i@raynes.me>
2012-06-26 00:30:20 -0500

    Add an exclusion for an old version of ring-core because it suddenly prefers it over the new version.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://refheap.com"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [stencil "0.2.0"]
-                 [noir "1.3.0-beta10"]
+                 [noir "1.3.0-beta11"]
                  [congomongo "0.1.9"]
                  [clj-config "0.2.0"]
                  [clj-http "0.4.0"]
                  [clavatar "0.2.0"]
                  [clj-time "0.3.3"]
-                 [amalloy/mongo-session "0.0.2"]
+                 [amalloy/mongo-session "0.0.2" :exclusions [ring/ring-core]]
                  [conch "0.3.0"]
                  [org.bovinegenius/exploding-fish "0.2.0"]]
   :main refheap.server)
