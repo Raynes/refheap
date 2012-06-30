5835a03a6c0cf0eb0991d734b5ce6b7a59ae4f39
Michael S. Klishin <michael@novemberain.com>
2012-06-30 21:53:22 +0400

    Add Monger, remove Exploding Fish, Mongo session store and Congomongo
    Monger has Ring session store implementation and supports URI connections out of the box

--- project.clj
+++ project.clj
@@ -5,13 +5,11 @@
                  [stencil "0.2.0"]
                  [noir "1.3.0-beta10"]
                  [lib-noir "0.2.0-alpha1"]
-                 [congomongo "0.1.9"]
+                 [com.novemberain/monger "1.0.0"]
                  [clj-config "0.2.0"]
                  [clj-http "0.4.0"]
                  [clavatar "0.2.0"]
                  [clj-time "0.3.3"]
-                 [amalloy/mongo-session "0.0.2" :exclusions [ring/ring-core]]
-                 [conch "0.3.0"]
-                 [org.bovinegenius/exploding-fish "0.2.0"]]
+                 [conch "0.3.0"]]
   :main refheap.server)
 
