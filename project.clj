1d12a5550703268a23a40feaa56da7ba7380b9bd
Michael Klishin <michael@defprotocol.org>
2012-07-27 15:19:56 +0400

    Bump Monger to 1.1.2

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [stencil "0.2.0"]
                  [noir "1.3.0-beta10"]
                  [lib-noir "0.2.0-alpha2"]
-                 [com.novemberain/monger "1.1.0-beta1"]
+                 [com.novemberain/monger "1.1.2"]
                  [clj-config "0.2.0"]
                  [clj-http "0.4.0"]
                  [clavatar "0.2.0"]
