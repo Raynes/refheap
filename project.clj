572b1416ed06af8b4d0ad8fc31d86e335a536ded
Michael Klishin <michael@defprotocol.org>
2012-12-03 20:40:40 +0400

    Monger 1.4.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [stencil "0.2.0"]
                  [compojure "1.1.3"]
                  [lib-noir "0.2.0"]
-                 [com.novemberain/monger "1.3.3"]
+                 [com.novemberain/monger "1.4.0"]
                  [clj-config "0.2.0"]
                  [clj-http "0.4.0"]
                  [clavatar "0.2.0"]
