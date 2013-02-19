850330c9d9b3129bec08d7fdef35d65ba29bb406
Anthony Grimes <i@raynes.me>
2013-02-18 23:25:57 -0800

    Inline these because they're very simple.

--- src/refheap/views/legal.clj
+++ src/refheap/views/legal.clj
@@ -3,9 +3,8 @@
             [compojure.core :refer [defroutes GET]] 
             [refheap.views.common :refer [layout]]))
 
-(def tos-page (layout (slurp (io/resource "refheap/views/templates/tos.html"))))
-(def privacy-page (layout (slurp (io/resource "refheap/views/templates/privacy.html"))))
-
 (defroutes legal-routes
-  (GET "/legal/tos" [] tos-page)
-  (GET "/legal/privacy" [] privacy-page))
+  (GET "/legal/tos" []
+    (-> "refheap/views/templates/tos.html" resource slurp layout))
+  (GET "/legal/privacy" []
+    (-> "refheap/views/templates/privacy.html" resource slurp layout)))
