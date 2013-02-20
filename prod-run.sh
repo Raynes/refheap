a278d6d748ecde4e8864c63128711f0ce7a38b54
Anthony Grimes <i@raynes.me>
2013-02-20 15:15:47 -0800

    Lower default memory usage.

--- prod-run.sh
+++ prod-run.sh
@@ -1,6 +1,6 @@
 export CANONICAL_HOST=www.refheap.com
 export HOSTS=refheap.com,www.refheap.com
-export JVM_OPTS="-Xmx400m"
+export JVM_OPTS="-Xmx60m"
 export MONGOLAB_URI=mongodb://127.0.0.1/refheap
 export MONGO_URI=mongodb://127.0.0.1/refheap
 export PORT=43535
