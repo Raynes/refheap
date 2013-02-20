f7f6033764cda1108ed340a92cd7d321694bbb89
Anthony Grimes <i@raynes.me>
2013-02-20 00:02:11 -0800

    Add prod-run.sh

---
+++ prod-run.sh
@@ -0,0 +1,9 @@
+export CANONICAL_HOST=www.refheap.com
+export HOSTS=refheap.com,www.refheap.com
+export JVM_OPTS="-Xmx400m"
+export MONGOLAB_URI=mongodb://127.0.0.1/refheap
+export MONGO_URI=mongodb://127.0.0.1/refheap
+export PORT=43535
+export LEIN_NO_DEV=t
+
+lein ring server-headless
