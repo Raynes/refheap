7d59b5d7f28b2eb98731a3b7a546d9756c903acf
Anthony Grimes <i@raynes.me>
2013-02-27 01:21:23 -0800

    Upgrade laser.

--- project.clj
+++ project.clj
@@ -7,13 +7,13 @@
                  [lib-noir "0.3.8"]
                  [com.novemberain/monger "1.5.0-beta1"]
                  [clj-config "0.2.0"]
-                 [clj-http "0.6.4"]
+                 [clj-http "0.6.4" :exclusions [org.jsoup/jsoup]]
                  [clavatar "0.2.1"]
                  [clj-time "0.4.4"]
                  [me.raynes/conch "0.5.1"]
                  [commons-codec/commons-codec "1.6"]
                  [me.raynes/cegdown "0.1.0"]
-                 [me.raynes/laser "0.1.30"]]
+                 [me.raynes/laser "1.1.1"]]
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler refheap.server/handler})
 
